cb7e1497afa38483d5e0e78f600d691633eac4a2
hasanulhasan <hasanulkarimh@gmail.com>
2024-10-21 22:51:26 +0600

    add image and header section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Components/header/Header";
 
 function App() {
   return (
-    <>
+    <div className="w-11/12 mx-auto">
+      <Header></Header>
       <h3>hi arfat kemon achi</h3>
-    </>
+    </div>
   );
 }
 
---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex justify-between items-center mt-5">
+        <h1 className="font-bold text-3xl">Knowledge Cafe</h1>
+        <img src="../../../images/profile.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
